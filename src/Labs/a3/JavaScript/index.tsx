343847ecbc18ff13591682c5c41c1d5d3470b17b
Connor Clancy <clancy.mu@gmail.com>
2024-02-11 13:59:12 -0500

    Complete Lab 2.2

--- src/Labs/a3/JavaScript/index.tsx
+++ src/Labs/a3/JavaScript/index.tsx
@@ -6,21 +6,30 @@ import BooleanVariables from "./variables/BooleanVariables";
 import VariableTypes from "./variables/VariableTypes";
 import WorkingWithArrays from "./arrays/WorkingWithArrays";
 import WorkingWithFunctions from "./functions/WorkingWithFunctions";
+import TemplateLiterals from "./string/TemplateLiterals";
+import House from "./json/House";
+import Spreading from "./json/Spreading";
+import Destructing from "./json/Destructing";
+import FunctionDestructing from "./json/FunctionDestructing";
 
 
 function JavaScript() {
     console.log('Hello World!');
     return(
        <div>
-          <h1>JavaScript</h1>
-          <VariablesAndConstants/>
-          <VariableTypes/>
-          <BooleanVariables/>
-          <IfElse/>
-          <TernaryOperator/>
-          <WorkingWithFunctions/>
-          <WorkingWithArrays/>
-
+         <h1>JavaScript</h1>
+         <VariablesAndConstants/>
+         <VariableTypes/>
+         <BooleanVariables/>
+         <IfElse/>
+         <TernaryOperator/>
+         <WorkingWithFunctions/>
+         <WorkingWithArrays/>
+         <TemplateLiterals/>
+         <House/>
+         <Spreading/>
+         <Destructing/>
+         <FunctionDestructing/>
        </div>
     );
 }
