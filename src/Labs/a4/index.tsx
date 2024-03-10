4b4f946bdf6d231ca4f9a9575fd6baf787890ef2
Connor Clancy <clancy.mu@gmail.com>
2024-03-10 16:41:12 -0400

    Add Lab 2.1 and 2.2

--- src/Labs/a4/index.tsx
+++ src/Labs/a4/index.tsx
@@ -1,8 +1,23 @@
-function Assignment4() {
-    return (
-      <div>
-        <h1>Assignment 4</h1>
-      </div>
-    );
+import React from "react";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import PassingFunctions from "./PassingFunctions";
+import EventObject from "./EventObject";
+
+const Assignment4 = () => {
+  function sayHello() {
+    alert("Hello");
   }
-  export default Assignment4;
+
+  return(
+    <>
+      <h1>Assignment 4</h1>
+      <PassingFunctions theFunction={sayHello} />
+      <ClickEvent/>
+      <PassingDataOnEvent/>
+      <EventObject/>
+    </>
+  );
+};
+
+export default Assignment4;
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,24 @@
-import {Link} from "react-router-dom";
-import Assignment3 from './a3';
 import Nav from "../Nav";
+import Assignment3 from "./a3";
+import Assignment4 from "./a4";
+import {Routes, Route, Navigate}
+  from "react-router";
 
 function Labs() {
-    return (
-       <div>
-            <Nav/>
-            <Assignment3 />
-       </div>
-    );
- }
+   return (
+      <div>
+         <Nav/>
+         <Routes>
+            <Route path="/"
+               element={<Navigate to="a3"/>}/>
+            <Route path="a3"
+               element={<Assignment3/>}/>
+            <Route path="a4"
+               element={<Assignment4/>}/>
+         </Routes>
+      </div>
+   );
+  }
+  
+
  export default Labs;
