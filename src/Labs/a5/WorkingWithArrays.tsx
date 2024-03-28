77d9ee24d7595fa8f6a6db52af4a4aa874c5fd4d
Connor Clancy <clancy.mu@gmail.com>
2024-03-28 19:26:58 -0400

    Finish Labs

--- src/Labs/a5/WorkingWithArrays.tsx
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -27,6 +27,9 @@ function WorkingWithArrays() {
     // create todos array setter
     const [todos, setTodos] = useState<Todo[]>([]);
 
+    // create error message setter
+    const [errorMessage, setErrorMessage] = useState(null);
+
     // get request for todos
     const fetchTodos = async () => {
         const response = await axios.get(API);
@@ -63,6 +66,29 @@ function WorkingWithArrays() {
         const response = await axios.post(API, todo);
         setTodos([...todos, response.data]);
     };    
+
+    // delete todo
+    const deleteTodo = async (todo: Todo) => {
+        try {
+            const response = await axios.delete(`${API}/${todo.id}`);
+            setTodos(todos.filter((t) => t.id !== todo.id));
+        } catch (error: any) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+        }  
+    };
+
+    // update todo
+    const updateTodo = async () => {
+        try{
+            const response = await axios.put(`${API}/${todo.id}`, todo);
+            setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+        } catch (error: any) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+        }
+    };
+    
     
     // load todos on page load
     useEffect(() => {
@@ -73,100 +99,66 @@ function WorkingWithArrays() {
       <div className="container">
         <h3>Working with Arrays</h3>
 
-        <h4>Retrieving Arrays</h4>
-        <a className="btn btn-primary" id="3.3.1"
-            href={API}>
-          Get Todos
-        </a>
-
-        <h4>Retrieving an Item from an Array by ID</h4>
-        <input className="form-control"
-            value={todo.id}
-            onChange={(e) => setTodo({ ...todo,
-            id: parseInt(e.target.value) })}/>
-        <a className="btn btn-primary" 
-            href={`${API}/${todo.id}`}>
-            Get Todo by ID
-        </a>
-
-        <h3>Filtering Array Items</h3>
-        <a className="btn btn-primary" id="3.3.2"
-            href={`${API}?completed=true`}>
-            Get Completed Todos
-        </a>
-
-        <h3>Creating new Items in an Array</h3>
-        <a className="btn btn-primary" id="3.3.4"
-            href={`${API}/create`}>
-            Create Todo
-        </a>
-
-        <h3>Deleting from an Array</h3>
-        <a className="btn btn-primary" id="3.3.5"
-            href={`${API}/${todo.id}/delete`}>
-            Delete Todo with ID = {todo.id}
-        </a>
-
-        <h3>Updating an Item in an Array</h3>
-        <input 
-            className="form-control"
-            type="text" 
-            value={todo.title}
-            onChange={(e) => setTodo({
-            ...todo, title: e.target.value })}/>
-        <a className="btn btn-primary"
-            href={`${API}/${todo.id}/title/${todo.title}`} >
-            Update Title to {todo.title}
-        </a>
-
-
-        
-        <h3>Updating Complete Flag</h3>
-        <input
-            id="completed"
-            type="checkbox"
-            checked={todo.completed}
-            onChange={(e) => setTodo({
-                ...todo, completed: Boolean(e.target.value) })}
-            />
-        <label htmlFor="completed">Completed</label>
-        <br/>
-        <a className="btn btn-primary"
-            href={`${API}/${todo.id}/completed/${todo.completed}`} >
-            Update Completed {todo.completed}
-        </a>
-
-        <h3>Updating Description</h3>
-        <textarea 
-            className="form-control"
+        <input className="form-control" 
+            value={todo.id} readOnly />
+        <input className="form-control" 
+            onChange={(e) => setTodo({ ...todo })}
+            value={todo.title} type="text" />
+        <textarea className="form-control" 
             value={todo.description}
+            onChange={(e) => setTodo({ ...todo,
+            description: e.target.value })} />
+        <input className="form-control"  
+            value={todo.due} type="date"
             onChange={(e) => setTodo({
-            ...todo, description: e.target.value })}/>
-        <a className="btn btn-primary"
-            href={`${API}/${todo.id}/description/${todo.description}`} >
-            Update description
-        </a>
+            ...todo, due: e.target.value })} />
+        <div className="form-check">
+            <input className="form-check-input"
+                checked={todo.completed} type="checkbox"
+                onChange={(e) => setTodo({
+                    ...todo, completed: e.target.checked })}
+            />
+            <label className="form-check-label">Completed</label>
+        </div>
 
         <div className="container">
+            <button className="btn btn-warning"
+                onClick={postTodo}> 
+                Post Todo 
+            </button>
+            <button className="btn btn-success" 
+                onClick={updateTodo}>
+                Update Todo
+            </button>
             <button className="btn btn-primary"
                 onClick={createTodo} >
                 Create Todo
             </button>
-            <button className="btn btn-success"
+            {/* <button className="btn btn-success"
                 onClick={updateTitle} >
                 Update Title
-            </button>
+            </button> */}
 
+            {errorMessage && (
+                    <div className="alert alert-danger mb-2 mt-2">
+                    {errorMessage}
+                    </div>
+            )}
 
             <ul className="list-group">
                 {todos.map((todo) => (
                     <li className="list-group-item" key={todo.id}>
+                        <input checked={todo.completed}
+                            type="checkbox" readOnly />
                         {todo.title}
-                        <button className="btn btn-danger"
-                            onClick={() => removeTodo(todo)}>
+                        <p>{todo.description}</p>
+                        <p>{todo.due}</p>
+
+                        <button onClick={() => deleteTodo(todo)}
+                            className="btn btn-danger float-end ms-2">
                             Remove
                         </button>
-                        <button className="btn btn-warning"
+                        <button className="btn btn-warning float-end ms-2"
                             onClick={() => fetchTodoById(todo.id)} >
                             Edit
                         </button>
