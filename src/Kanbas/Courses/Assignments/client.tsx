243e7ebabf01edb4365c88d864eca17990be1fc4
conclancy <47197778+conclancy@users.noreply.github.com>
2024-04-10 15:21:09 -0400

    Update client.tsx
    Fix assignments API const

--- src/Kanbas/Courses/Assignments/client.tsx
+++ src/Kanbas/Courses/Assignments/client.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
 const COURSES_API =  `${API_BASE}/api/courses`;
-const ASSIGNMENTS_API = `${API_BASE}/api/modules`;
+const ASSIGNMENTS_API = `${API_BASE}/api/assignments`;
 
 // GET all assignments by courseId 
 export const findAssignmentsForCourse = async (courseId: any) => {
